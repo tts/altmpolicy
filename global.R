c85688415a00ecc65f86a02473ca0689f036d1e7
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2016-12-29 13:47:59 +0200

    Renamed dataframe

--- global.R
+++ global.R
@@ -5,22 +5,23 @@ library(DT)
 library(ggiraph)
 library(ggplot2)
 
-# Data for charts and DT datatable
-dataForCharts <- read.table(file = "metrics.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+data <- read.table(file = "metrics.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
 
-names(dataForCharts) <- c("Altmetric_URL", "Journal", "Title", "Altmetric", "Mendeley", "CiteULike", 
+names(data) <- c("Altmetric_URL", "Journal", "Title", "Altmetric", "Mendeley", "CiteULike", 
                           "Readers", "GPlus", "Facebook", "Posts", "Twitter",
                           "Accounts", "Feeds", "YouTube", "Reddit", 
                           "ResearchForums", "NewsOutlets", "Policy", "Year", "Cites", "School")
 
-dataForCharts$Title <- dataForCharts$Title <- gsub("'","", dataForCharts$Title)
-dataForCharts$Title[dataForCharts$Title == "Long-term sea-level rise implied by 1.5[thinsp][deg]C and 2[thinsp][deg]C warming levels"] <- "Long-term sea-level rise implied by 1.5C and 2C warming levels"
-dataForCharts$Title[dataForCharts$Title == "Early Prediction of Movie Box Office Success based on Wikipedia Activity\n  Big Data"] <- "Early Prediction of Movie Box Office Success based on Wikipedia Activity Big Data"
+# Bad data
+data$Title <- data$Title <- gsub("'","", data$Title)
+data$Title[data$Title == "Long-term sea-level rise implied by 1.5[thinsp][deg]C and 2[thinsp][deg]C warming levels"] <- "Long-term sea-level rise implied by 1.5C and 2C warming levels"
+data$Title[data$Title == "Early Prediction of Movie Box Office Success based on Wikipedia Activity\n  Big Data"] <- "Early Prediction of Movie Box Office Success based on Wikipedia Activity Big Data"
 
-dataForCharts <- dataForCharts %>% 
+data <- data %>% 
   mutate(Link = paste0("<a href='", Altmetric_URL, "'>", substr(Altmetric_URL, 47, nchar(Altmetric_URL)), '</a>'))
 
-dataForCharts <- dataForCharts[ ,c(ncol(dataForCharts),1:ncol(dataForCharts)-1)]
+# Reorder columns
+data <- data[ ,c(ncol(data),1:ncol(data)-1)]
 
 metrics_select <- sort(c("Altmetric", "Mendeley", "Twitter", "Facebook", "GPlus", "CiteULike", "Readers", "Posts", "Accounts", "Cites", "Feeds",
              "YouTube", "Reddit", "NewsOutlets", "ResearchForums", "Policy"))
